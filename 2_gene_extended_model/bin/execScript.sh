3eec19df2997f9c9b33ff8ca052a1163ee83b0fc
Ashley Teufel <ateufel@utexas.edu>
2019-08-19 10:52:04 -0600

    Update execScript.sh
    fixed genpath error

--- 2_gene_extended_model/bin/execScript.sh
+++ 2_gene_extended_model/bin/execScript.sh
@@ -6,4 +6,4 @@
 #! echo -n $README > README.txt
 #sudo nohup /usr/local/MATLAB/R2016a/bin/glnxa64/MATLAB -singleCompThread -r "cd(pwd);addpath(genpath('../'));disp(pwd);ProjectScript"
 #nohup /usr/bin/matlab -singleCompThread -r "cd(pwd);addpath(genpath('../'));disp(pwd);ProjectScript"
-nohup /stor/system/opt/MATLAB/R2015b/bin/matlab -singleCompThread -r "cd(pwd);addpath(genpath(',,/'));disp(pwd);ProjectScript"
+nohup /stor/system/opt/MATLAB/R2015b/bin/matlab -singleCompThread -r "cd(pwd);addpath(genpath('../'));disp(pwd);ProjectScript"
